e3d026313ba20bf29c649c7431ceff2139e31e10
Russell Coleman <rootofsilence@gmail.com>
2023-03-04 14:21:56 +0000

    updated card compoent and project page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-//import "./style.css";
+//import "./style.css"
+
 
 function Card(props) {
   return (
-    <div class="card">
+    <div class="card col-md-4">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -13,6 +14,8 @@ function Card(props) {
 }
 
 export default Card;
+
+ 
 
 
 /*
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,9 +4,14 @@ import Card from "../components/Card";
 
 function Projects() {
   return (
-    <div>
+    <div class="container">
+            <div class="row hidden-md-up">
+
       <h2>Projects</h2>
       <Card />
+      <Card />
+      <Card />
+            </div> 
     </div>
   );
 }
