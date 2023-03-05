c802eac8ee6a3a4bdf058420e26b90797cf64fe8
Russell Coleman <rootofsilence@gmail.com>
2023-03-05 21:08:10 +0000

    added contact form, formatted social media buttons, tweaked background image styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,18 +3,43 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 
-
 function Contact() {
+
   return (
-    <div className="container text-centre" id="contact">
+    <div className="container" id="contact">
       <h2>Contact</h2>
 
-      <ul>
-    <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faGithub} /></a></li>
-    <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-    <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faEnvelope} /></a></li>
-</ul>
-    </div>
+      <form className="justify-content-center">
+  <div className="form-outline mb-4">
+    <input type="text" id="form4Example1" className="form-control"placeholder="Name" />
+  </div>
+ 
+  <div className="form-outline mb-4">
+    <input type="email" id="form4Example2" className="form-control" placeholder="Email"></input>
+  </div>
+
+  <div className="form-outline mb-4">
+    <textarea className="form-control" id="form4Example3" rows="4" placeholder="Message"></textarea>
+  </div>
+
+  <button type="submit" className="btn btn-dark btn-block mb-4">Send</button>
+</form>
+
+  <div className="text-center">
+  <section className="social-media">
+  <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/rus-coleman/" role="button">
+  <FontAwesomeIcon icon={faLinkedin} className="fa-2xl"/>
+  </a>
+  <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/rcm6" role="button">
+  <FontAwesomeIcon icon={faGithub} className="fa-2xl"/>
+  </a>
+  <a class="btn btn-outline-light btn-floating m-1" href="mailto:Demo@notrealaddress.com" role="button">
+  <FontAwesomeIcon icon={faEnvelope} className="fa-2xl"/>
+  </a>
+  </section>
+  </div>
+
+  </div>
   );
 }
 
