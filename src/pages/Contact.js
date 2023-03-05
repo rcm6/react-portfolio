55d1052ab30e585cf042396f6101a1909e1fd040
Russell Coleman <rootofsilence@gmail.com>
2023-03-05 18:04:42 +0000

    updated contact page - ebvelope icon

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faTwitter, faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 
 
 function Contact() {
@@ -11,7 +12,7 @@ function Contact() {
       <ul>
     <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faGithub} /></a></li>
     <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-    <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faTwitter} /></a></li>
+    <li><a href="http://github.com/rcm6"><FontAwesomeIcon icon={faEnvelope} /></a></li>
 </ul>
     </div>
   );
